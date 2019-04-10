7963fc7745b0a3331f653ea046a3856032825b92
JingChen <jingchenjc2019@gmail.com>
2019-04-10 01:28:06 -0500

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(array){
+  for(let i = 0; i < array.length; i++){
+    let object = array.find(object => object.result === "W");
+    if(object){
+      return object.year;
+    }else{
+      return undefined;
+    }
+  }
+}
